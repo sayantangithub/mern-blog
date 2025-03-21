a3774ce6afbbe76b8048a1707f199cb53b821340
sayantan.bhattacharya16@gmail.com <sayantan.bhattacharya16@gmail.com>
2025-03-21 12:56:54 +0530

    Create signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controller/auth.controller.js";
+import { signIn, signup } from "../controller/auth.controller.js";
 
 const AuthRoute = express.Router();
 
 AuthRoute.post("/signup", signup);
+AuthRoute.post("/signin", signIn);
 
 export default AuthRoute;
