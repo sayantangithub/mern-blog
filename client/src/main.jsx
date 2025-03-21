3ff74b1a9d4ac48c915cbffac23b46112fef37aa
sayantan.bhattacharya16@gmail.com <sayantan.bhattacharya16@gmail.com>
2025-03-21 20:25:37 +0530

    complete dark mode functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { persistor, store } from './redux/store.js'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </PersistGate>
-)
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-import {persistReducer, persistStore } from "redux-persist";
+import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import themeReducer from "./theme/themeSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
