71ff517c741b701735104a6b9a508e7ad41f466d
sayantan.bhattacharya16@gmail.com <sayantan.bhattacharya16@gmail.com>
2025-03-23 09:07:31 +0530

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAd5avdba8EiOZH8lmV3XshrXx7dKRZvhx-A&s",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
