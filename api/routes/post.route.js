d94904e62c8bacb9d59c5a8f6e6502ff98c1e50c
sayantan.bhattacharya16@gmail.com <sayantan.bhattacharya16@gmail.com>
2025-03-24 00:30:42 +0530

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create } from "../controller/post.controller.js";
+import { create, getPosts } from "../controller/post.controller.js";
 
 const PostRouter = express.Router();
 PostRouter.post("/create", verifyToken, create);
+PostRouter.get("/getposts", getPosts);
 
 export default PostRouter;
