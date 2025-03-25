6329d9d0d5330f6af3b993306bbbcda58aaba881
sayantan.bhattacharya16@gmail.com <sayantan.bhattacharya16@gmail.com>
2025-03-25 13:12:03 +0530

    show the comment of a post

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createComment } from "../controller/comment.controller.js";
+import {
+  createComment,
+  getPostComment,
+} from "../controller/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const CommentRouter = express.Router();
 
-CommentRouter.post("/create",verifyToken, createComment);
+CommentRouter.post("/create", verifyToken, createComment);
+CommentRouter.get("/getPostComments/:postId", getPostComment);
 
 export default CommentRouter;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUsers,
   signout,
   updateUser,
@@ -16,5 +17,6 @@ UserRouter.put("/update/:userId", verifyToken, updateUser);
 UserRouter.delete("/delete/:userId", verifyToken, deleteUser);
 UserRouter.post("/signout", verifyToken, signout);
 UserRouter.get("/getusers", verifyToken, getUsers);
+UserRouter.get("/:userId", getUser);
 
 export default UserRouter;
